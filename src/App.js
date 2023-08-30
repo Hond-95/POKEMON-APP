8248f1ce4267ca631d6c7e52aa98ab03e58bcafb
Hond-95 <honda@scottlab.org>
2023-08-30 19:55:39 +0900

    fetch pokemons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
+import { useState, useEffect } from 'react';
+import Card from './components/Card/Card';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
 import './App.css';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon"
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+
+      // 各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      {loading ? (
+        <h1>ロード中・・・・</h1>
+      ) : (
+        <>
+          <div className='pokemonCardContainer'>
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
